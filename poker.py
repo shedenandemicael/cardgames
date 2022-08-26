db59347ad9f888f1270b5d2710836eedc10b70cb
Sheden Andemicael <shedenandemicael@Shedens-Air.lan>
2022-08-26 18:09:50 -0500

    completed betting for all rounds

--- poker.py
+++ poker.py
@@ -3,23 +3,38 @@ from random import shuffle
 
 suits = ('diamond', 'heart', 'spade', 'club')
 numbers = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-pot = 0
 currentBet = 0
 bigBlind = 20
 stackSize = 1000
+pot = bigBlind + (bigBlind // 2)
 
 def main():
 
+    # Game setup
     deck = fillDeck()
     players = int(input("how many player? "))
     while players < 3 and players > 8:
         players = int(input("how many player? "))
     hands = dealCards(deck, players)
+
+    # Pre-flop betting begins after big blind
     player = 2
-    placeBets(hands, player, players)
+    player = placeBets(hands, player, players)
+
+    # Betting on the flop
     flop = deck[players * 2 + 1:players * 2 + 4]
+    updateHands(hands, flop)
+    player = placeBets(hands, player, players)
+
+    # Betting on the turn
     turn = deck[players * 2 + 6]
+    updateHands(hands, turn)
+    player = placeBets(hands, player, players)
+
+    # Betting on the river
     river = deck[players * 2 + 8]
+    updateHands(hands, river)
+    player = placeBets(hands, player, players)
 
 
 class Player:
@@ -62,21 +77,33 @@ def dealCards(deck, players):
 
 def placeBets(hands, player, players):
 
-    while hands[player].playerBet != currentBet:
+    # Ensures everyone goes at least once
+    global currentBet
+    turns = 0
+    while hands[player].playerBet != currentBet or turns < players:
         decision = input("what will you do? ")
         player, players = handDecision(decision, hands, player, players)
-        print(player)
-        print(players)
         if player == players - 1:
             player = 0
         else:
             player += 1
+        turns += 1
+        print(player)
+
+    # Reset bets
+    for hand in hands:
+        hand.playerBet = 0
+    currentBet = 0
+    return 0
 
 
 def handDecision(decision, hands, player, players):
 
     global currentBet
+    global pot
+
     if decision == "call":
+        pot += (currentBet - hands[player].playerBet)
         hands[player].playerBet = currentBet
     
     elif decision == "fold":
@@ -85,10 +112,26 @@ def handDecision(decision, hands, player, players):
         players -= 1
 
     elif decision == "raise":
-        raiseAmount = int(input("raise amount "))
+        raiseAmount = int(input("raise amount? "))
+        pot += (raiseAmount - hands[player].playerBet)
         currentBet = hands[player].playerBet = hands[player].playerBet + raiseAmount
-    
+
+    elif decision == "bet":
+        currentBet = hands[player].playerBet = betAmount = int(input("bet amount? "))
+        pot += betAmount
+
+    elif decision == "check":
+        return player, players
+
     return player, players
+
+
+def updateHands(hands, round):
+
+    # Adding community cards to players' hands
+    for player in hands:
+        player.hand.append(round)
         
+
 if __name__ == '__main__':
     main()
